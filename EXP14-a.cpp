543a31e96594cdb67c14dd26af2a69107375facd
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-04 09:59:09 +0530

    Create EXP14-a.cpp

---
+++ EXP14-a.cpp
@@ -0,0 +1,33 @@
+//Dhairya Thakkar
+//24070123037
+//A2
+//Single inheritance
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class car {
+    public:
+    string a ="skoda ";
+    void model(){
+        cout<<"SUV"<<endl;
+    }
+};
+class name: public car {
+    public:
+    string b ="octavia";
+};
+
+int main(){
+    name u1;
+    u1.model();
+    cout<<u1.a ;
+    cout<< u1.b;
+}
+
+
+/* OUTPUT:
+SUV
+skoda octavia
+*/
