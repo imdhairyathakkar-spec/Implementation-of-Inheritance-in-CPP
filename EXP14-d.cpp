aa0866aad0943d547ee80b3d5446be5b07d593a8
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-04 10:02:17 +0530

    Create EXP14-d.cpp

---
+++ EXP14-d.cpp
@@ -0,0 +1,59 @@
+// Dhairya Thakkar
+// 24070123037
+// A2
+// Hierarchical Inheritance
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Brand {
+protected:
+    string brand;
+public:
+    Brand() {
+        brand = "Tata Motors";
+    }
+    void showBrand() {
+        cout << "Brand: " << brand << endl;
+    }
+};
+
+class Carr : public Brand {
+    string model;
+public:
+    Carr() {
+        cout << "Enter Model: ";
+        cin >> model;
+    }
+    void display() {
+        showBrand();
+        cout << "Model: " << model << endl;
+    }
+};
+
+class Features : public Brand {
+public:
+    void classic() {
+        showBrand();
+        cout << "Features: Safety, Comfort, and Fuel Efficiency" << endl;
+    }
+};
+
+int main() {
+    Carr c;
+    c.display();
+
+    Features f;
+    f.classic();
+
+    return 0;
+}
+
+/* OUTPUT:
+Enter Model: Nexon
+Brand: Tata Motors
+Model: Nexon
+Brand: Tata Motors
+Features: Safety, Comfort, and Fuel Efficiency
+*/
